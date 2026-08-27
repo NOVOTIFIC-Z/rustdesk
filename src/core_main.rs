df7c9120c4b3351a6cb75671c2577900f89c4a4b
NOVOTIFIC-Z <daocheng.zheng@novotific.com>
2026-08-28 03:01:26 +0800

    屏蔽托盘图标

--- src/core_main.rs
+++ src/core_main.rs
@@ -378,7 +378,8 @@ pub fn core_main() -> Option<Vec<String>> {
             }
         } else if args[0] == "--tray" {
             if !crate::check_process("--tray", true) {
-                crate::tray::start_tray();
+                 //屏蔽托盘图标
+                //crate::tray::start_tray();
             }
             return None;
         } else if args[0] == "--install-service" {
@@ -414,7 +415,8 @@ pub fn core_main() -> Option<Vec<String>> {
             #[cfg(target_os = "macos")]
             {
                 let handler = std::thread::spawn(move || crate::start_server(true, false));
-                crate::tray::start_tray();
+                //屏蔽托盘图标
+                //crate::tray::start_tray();
                 // prevent server exit when encountering errors from tray
                 hbb_common::allow_err!(handler.join());
             }
